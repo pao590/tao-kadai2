9955be90bac79e1371d7f31f316a727300183df0
[pao] <gorirando39@gmail.com>
2025-05-09 07:44:14 +0900

    課題提出

--- src/routes/web.php
+++ src/routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ProductController;
 
 Route::get('/', [ProductController::class, 'index'])->name('products.index');
 
+Route::get('/products', [ProductController::class, 'index']);
+
 Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
 
 Route::post('/products', [ProductController::class, 'store'])->name('products.store');
